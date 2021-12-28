86279a6e50fcdc56ad7ffc9e12570c3fb0b28f51
IGICOCORO <nduwimanaguymich@gmail.com>
2021-12-28 07:02:21 -0800

    created serializers

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import *
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = '__all__'
+
+
+class ChambreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chambre
+        fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+    
+class TokenPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super(TokenPairSerializer, self).validate(attrs)
+        data['id'] = self.user.id
+        data['first_name'] = self.user.first_name
+        data['last_name'] = self.user.last_name
+
+        return data
---
+++ api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import *
+
+router = routers.DefaultRouter()
+
+router.register("hotel", HotelViewset)
+router.register("chambre", ChambreViewset)
+router.register("Reservation", ReservationViewset)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login/', TokenPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+]
