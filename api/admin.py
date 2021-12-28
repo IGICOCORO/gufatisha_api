40e1c9bde23c82f970e2a0375618e47402d67c5c
IGICOCORO <nduwimanaguymich@gmail.com>
2021-12-28 06:13:21 -0800

    registered models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+	list_display = "nom", "photo_couverture", "chambre"
+	search_fields = "nom", 
+    
+@admin.register(Chambre)
+class ChambreAdmin(admin.ModelAdmin):
+	list_display = "pic_chambr1", "pic_chambr2", "pic_chambr3", "date_arrivee", "date_depart", "nbre_personnes"
+	search_fields = "nbre_personnes",
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+	list_display = "user", "chambre"
+	search_fields = "user__first_name", 
