4cb5ff663f43f999e20bc84966ba7bf2dcee0993
IGICOCORO <nduwimanaguymich@gmail.com>
2021-12-28 07:01:44 -0800

    made views and api url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import *
+from .models import *
+
+class TokenPairView(TokenObtainPairView):
+	serializer_class = TokenPairSerializer
+
+class HotelViewset(viewsets.ModelViewSet):
+	serializer_class = HotelSerializer
+	queryset = Hotel.objects.all()
+	authentication_classes = [JWTAuthentication, SessionAuthentication]
+	permission_classes = [IsAuthenticated]
+
+
+class ChambreViewset(viewsets.ModelViewSet):
+	serializer_class = ChambreSerializer
+	queryset = Chambre.objects.all()
+	authentication_classes = [JWTAuthentication, SessionAuthentication]
+	permission_classes = [IsAuthenticated]
+
+
+class ReservationViewset(viewsets.ModelViewSet):
+	serializer_class = ReservationSerializer
+	queryset = Reservation.objects.all()
+	authentication_classes = [JWTAuthentication, SessionAuthentication]
+	permission_classes = [IsAuthenticated]
